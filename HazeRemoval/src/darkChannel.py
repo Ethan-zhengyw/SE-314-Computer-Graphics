2a07232e0cc09f01e7f62054557726d25839c258
Ethan-zhengyw <zhengyw55@gmail.com>
2015-03-17 20:41:10 +0800

    finished the left part of the algorithm

--- HazeRemoval/src/darkChannel.py
+++ HazeRemoval/src/darkChannel.py
@@ -2,6 +2,36 @@ import numpy as np
 from scipy import misc
 
 
+def get_hazy_free(hazy, ml, atmos):
+    row, col = ml.shape
+    hazy_free = np.array(hazy)
+
+    for i in range(row):
+        for j in range(col):
+            for c in range(3):
+                hazy_free[i, j, c] = (hazy[i, j, c] - ml[i, j]) / (1 - ml[i, j] / atmos)
+
+    return hazy_free
+
+
+def get_atmos(mave, mmax, epsilon):
+
+    return epsilon * mmax.max() + (1 - epsilon) * mave.max()
+
+
+def get_ml(mmin, mave, mav):
+    row, col = mmin.shape
+    ml = np.zeros((row, col))
+    rho = 1.2  # random.uniform(0, 1 / mav)
+    min_rm = min(rho * mav, 0.9)
+
+    for i in range(row):
+        for j in range(col):
+            ml[i, j] = min(min_rm * mave[i, j], mmin[i, j])
+
+    return ml
+
+
 def get_mm(hazy):
     row, col = hazy.shape[0], hazy.shape[1]
     mmin = np.zeros((row, col))
@@ -26,6 +56,7 @@ def get_mave(mmin, k):
             cl = (j - k) if (j - k) >= 0 else 0
             cu = (j + k + 1) if (j + k + 1) <= col else col
 
+            # n = (ru - rl + 1) * (cu - cl + 1)
             mave[i, j] = mmin[rl:ru, cl:cu].mean()
 
     return mave
@@ -62,9 +93,19 @@ def main():
     for id_img in id_imgs:
         print(id_img)
         hazy = misc.imread('../images/hazy/%d.png' % id_img)
+        # print(hazy[0:10, 0:10, :])
         matrix_min, matrix_max = get_mm(hazy)
+        # print(matrix_min[0:10, 0:10])
         matrix_ave = get_mave(matrix_min, k)
+        # print(matrix_ave[0:10, 0:10])
         mav = matrix_min.mean() / 255
 
+        ml = get_ml(matrix_min, matrix_ave, mav)
+        atmos = get_atmos(matrix_min, matrix_ave, 0.05)
+        # print('atmos: ' + str(atmos))
+        hazy_free = get_hazy_free(hazy, ml, atmos)
+
+        misc.imsave('../images/dark_channel/%d.png' % id_img, matrix_ave, 'png')
+        misc.imsave('../images/hazy_free/%d.png' % id_img, hazy_free, 'png')
 
 main()
